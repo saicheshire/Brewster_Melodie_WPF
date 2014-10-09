babcd83db00016325548ce89fef561aadd0e3bbf
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-09 18:23:31 -0500

    Created Assignment: Expressions files
    Created necessary files and directories for Assignment: Expressions

---
+++ Expressions_Industry/js/industry.js
@@ -0,0 +1,3 @@
+/**
+ * Created by melodiebrewster on 10/9/14.
+ */
---
+++ Expressions_Personal/js/personal.js
@@ -0,0 +1,8 @@
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Assignment: Personal Expressions
+ 10-09-2014
+ */
+
+//Create a js file that calculates how many bags of dog food you go through in a year.
