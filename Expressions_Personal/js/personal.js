545194c0b44e794d980c23cf303e2783a25d9638
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-09 18:53:45 -0500

    Calculate Length
    Calculate Length

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -13,4 +13,9 @@ var name = prompt("Please type in your name:");
 //Say hello to the user and let them know what we are doing
 alert("Welcome "+name+"! We are going to figure out how many rats you should be able to fit in your cage.");
 
-console.log(name);
+//Ask how long the cage is in inches.
+//Create a variable for the answer.
+var length = prompt("How many inches long is the cage?");
+
+//Console.log the response
+console.log(length);
