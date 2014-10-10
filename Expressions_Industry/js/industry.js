60c279e325e06a4268de128b6af1c9f7340e3535
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-09 20:25:31 -0500

    Edited .html and .js files
    Edited .html and .js files

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -1,3 +1,9 @@
-/**
- * Created by melodiebrewster on 10/9/14.
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Assignment: Expressions (Industry)
+ 10-09-2014
  */
+
+//Create a js file that will tell you how far to move the yellow corner on an information bubble from one icon to another when mousing over the icon.
+
