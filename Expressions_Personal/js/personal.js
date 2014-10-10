7400415a3d8d3c4c7ecaa9bf1a42c1f2aaf23664
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-09 19:18:28 -0500

    Ask for Height and Calculate Cubic Inches
    Ask for Height and Calculate Cubic Inches

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -26,3 +26,20 @@ var width = prompt("How many inches wide is the cage?");
 
 //Console.log the response
 console.log(width);
+
+//Ask the user how tall the cage is in inches.
+//Create a variable for the answer.
+var height = prompt("How many inches tall is the cage?");
+
+//Console.log the response
+console.log(height);
+
+//Calculate the number of rats the cage can hold (each rat requires 2 cubic feet of space).
+//First we need to calculate how many cubic feet the cage is
+//Cubic feet is measured by multiplying the height * width * depth, then dividing that total by the number of cubic inches in a cubic foot.
+//A cubic foot is 12 * 12 * 12 = 1728
+//Create variables for our calculation.
+var totalSides = length * width * height;
+
+//Console.log totalSides
+console.log(totalSides);
