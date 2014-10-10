cd946d750ed55b20804eb27c6500db3a955c05ad
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-09 21:50:23 -0500

    Created answer to first question
    Created answer to first question

--- Expressions_Wacky/js/wacky.js
+++ Expressions_Wacky/js/wacky.js
@@ -1,3 +1,16 @@
-/**
- * Created by melodiebrewster on 10/9/14.
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Assignment: Expressions (Wacky)
+ 10-09-2014
  */
+
+//Calculate the average number of monkeys you have in each barrel
+//Ask the user how many monkeys are in the first barrel
+//Create a variable to show the answer
+
+var monkeyBarrel = new Array();
+monkeyBarrel[0] = prompt("How many monkeys are in the first barrel?");
+
+//Console.log the answer
+console.log(monkeyBarrel[0]);
