4586d2f77bc161372acbda4005b6439c4e07df8d
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-16 18:35:06 -0500

    Variables
    Created variables for Personal Conditionals

--- Conditionals_Personal/js/conpersonal.js
+++ Conditionals_Personal/js/conpersonal.js
@@ -1,3 +1,28 @@
-/**
- * Created by melodiebrewster on 10/16/14.
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Assignment Week 3: Conditionals - Personal
+ 10-16-2014
  */
+
+//Decide whether I should take my dogs for a walk, pending the temperature
+
+//Create variables to determine the current temperature
+var currentTemp = prompt("What is the temperature outside?");
+
+//Console log the current temperature
+console.log(currentTemp);
+
+//Create a variable to determine your minimum temperature
+var minTemp = prompt("What temperature is too cold for you to want to walk your dogs?");
+
+//Console log the minimum temperature
+console.log(minTemp);
+
+//Create a variable to determine your maximum temperature
+var maxTemp = prompt("What temperature is too hot for you to want to walk your dogs?");
+
+//Console log the maximum temperature
+console.log(maxTemp);
+
+
