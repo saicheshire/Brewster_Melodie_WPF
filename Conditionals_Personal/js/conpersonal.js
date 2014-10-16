9ad6e11b05e6af2d71cc475ce7cd106d7d2b4671
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-16 18:46:33 -0500

    Validations
    Created validations for prompts

--- Conditionals_Personal/js/conpersonal.js
+++ Conditionals_Personal/js/conpersonal.js
@@ -13,11 +13,23 @@ var currentTemp = prompt("What is the temperature outside?");
 //Console log the current temperature
 console.log(currentTemp);
 
+//validate prompt
+if (currentTemp === "") { //if the current temperature is left blank, then re prompt
+
+    name = prompt("Please enter the current temperature.  Don't leave it blank!");
+}
+
 //Create a variable to determine your minimum temperature
 var minTemp = prompt("What temperature is too cold for you to want to walk your dogs?");
 
 //Console log the minimum temperature
 console.log(minTemp);
+
+//validate prompt
+if (minTemp === "") { //if the current temperature is left blank, then re prompt
+
+    name = prompt("Please enter the minimum temperature.  Don't leave it blank!");
+}
 
 //Create a variable to determine your maximum temperature
 var maxTemp = prompt("What temperature is too hot for you to want to walk your dogs?");
@@ -25,4 +37,8 @@ var maxTemp = prompt("What temperature is too hot for you to want to walk your d
 //Console log the maximum temperature
 console.log(maxTemp);
 
+//validate prompt
+if (maxTemp === "") { //if the current temperature is left blank, then re prompt
 
+    name = prompt("Please enter the maximum temperature.  Don't leave it blank!");
+}
