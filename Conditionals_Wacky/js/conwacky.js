a0caa4be70a9cc45c3f16dd8283a8a0f276b4e98
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-16 20:03:52 -0500

    Summary and first variable/validation
    Created a summary for what we’re doing and created a variable then
    validated response and console logged it

--- Conditionals_Wacky/js/conwacky.js
+++ Conditionals_Wacky/js/conwacky.js
@@ -1,3 +1,20 @@
-/**
- * Created by melodiebrewster on 10/16/14.
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Conditionals Assignment: Wacky
+ 10-16-2014
  */
+
+//Determine whether your bitch (bitch=female dog) will receive a major when entered into a dog show
+//A major is 3, 4, or 5 points.  Majors are required to receive a champion title on your dog or bitch
+//Since the requirements differ between sexes (and breeds), we will be using a bitch as an example
+//Create variables to determine how many bitches need to attend in order for yours to receive a major and how many
+//bitches are actually attending the show.
+var minBitches = prompt("What is the minimum number of bitches required in order for you to get a major?");
+//validate response
+if (minBitches === ""){ //if answer is blank, then re-prompt the user
+
+    name = prompt("Please enter the minimum number.  Don't leave it blank!");
+}
+//Console log response
+console.log(minBitches);
