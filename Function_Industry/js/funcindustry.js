eb87a55b134d231765d76f8c95983d10be0b7684
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-23 21:57:23 -0500

    First Variable
    Created first variable and validation

--- Function_Industry/js/funcindustry.js
+++ Function_Industry/js/funcindustry.js
@@ -1,3 +1,17 @@
-/**
- * Created by melodiebrewster on 10/23/14.
+/*
+ Melodie Brewster
+ WPF 1410 Section 02
+ Functions Assignment - Industry
+ 10-22-2014
  */
+
+//Calculate an estimate the length and cost it will be to create a website for a client
+//create variable for hours spent on each page
+
+var perPage = prompt("How many hours on average does it take for you to finish each page?");
+
+//validate response, badger them for a number if you have to
+while(isNaN(perPage) || perPage===""){
+
+    perPage = prompt("please enter a number!");
+}
