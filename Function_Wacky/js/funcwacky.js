e5dfa9ec6853afcd781b2f89d9ff77ea53709b8c
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-23 22:40:17 -0500

    results
    created function, posted results to GitHub

--- Function_Wacky/js/funcwacky.js
+++ Function_Wacky/js/funcwacky.js
@@ -10,7 +10,27 @@
 var dogName = prompt("What is your dog's name?");
 
 //validate response, keep asking until a name is provided
-while(isNaN(dogName) || dogName===""){
+while(dogName===""){
 
     dogName = prompt("please enter your dog's name!");
+}
+
+//create variable for dog's gender
+var gender = prompt("Is your dog a male or a female?");
+
+//validate
+while(gender===""){
+
+    gender = prompt("please enter your dog's name!");
+}
+
+//create if statements
+if (gender == "male"){
+    console.log(dogName + " will most likely not have to stay overnight.  We'll call and let you know.");
+    alert(dogName + " will most likely not have to stay overnight.  We'll call and let you know.");
+}
+
+else {
+    console.log(dogName + " will have to stay overnight.");
+    alert(dogName + " will have to stay overnight.");
 }
