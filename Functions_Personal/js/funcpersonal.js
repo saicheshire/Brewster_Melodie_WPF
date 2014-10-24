e0e6c827a24e001c6a3a7c273ff7ad6a1215d3ea
saicheshire <melodiebrewster@Melodies-MacBook-Pro.local>
2014-10-23 21:44:28 -0500

    Lots of corrections & Submission
    Made some errors but worked through them and submitted result to github

--- Functions_Personal/js/funcpersonal.js
+++ Functions_Personal/js/funcpersonal.js
@@ -6,10 +6,29 @@
  */
 
 //We're going to calculate how often we need to buy a bag of dog food
+//set a variable for cups per day fed
 var cupsDay = prompt("How many cups do your dog/dogs eat per day?  Please enter amounts in decimals (i.e. 1 1/2 cups is 1.25).");
 
 //validate response, keep asking until a number is provided
-while(isNaN(min) || min===""){
+while(isNaN(cupsDay) || cupsDay===""){
 
-    min = prompt("please enter a number!");
+    cupsDay = prompt("please enter a number!");
+}
+
+//set a variable for size of the bag of food
+var foodBag = prompt("How many pounds does the bag weigh?");
+
+//validate response, keep asking until a number is provided
+while(isNaN(foodBag) || foodBag==="") {
+
+    foodBag = prompt("please enter a number!");
+}
+
+//set variable for total days the dog food bag will last
+var totalDays = calculateTotal(cupsDay,foodBag);
+console.log("You need to buy a bag of dog food every " + totalDays + " days.");
+
+//create function
+function calculateTotal(cupsDay,foodBag){
+    return foodBag * 4 / cupsDay;
 }
